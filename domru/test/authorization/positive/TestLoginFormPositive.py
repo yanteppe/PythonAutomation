1eb01653f7cf14f114c82658b009ee2a70eacf84
Yan <yanteppe@gmail.com>
2018-09-17 12:07:03 +0500

    add new test TestLoginFormPositive.py

--- domru/test/authorization/positive/TestLoginFormPositive.py
+++ domru/test/authorization/positive/TestLoginFormPositive.py
@@ -8,82 +8,157 @@ from domru.core.pageobject.LoginFormPage import LoginFormPage
 Проверка формы авторизации.
 Позитивные проверки полей username и password.
 '''
-class TestLoginFormPositive(TestBase):
+def testUsernameAndPassword_login_1(setUp):
+    loginForm = LoginFormPage(setUp)
+    uiHelper = UiHelper(setUp)
 
-    def testUsernameAndPassword_1(self):
-        self.getUrl("http://tvnew.domru.ru/")
-        uiHelper = UiHelper(self.driver)
-        loginForm = LoginFormPage(self.driver)
+    loginForm.openLoginForm()
+    loginForm.inputUserName("test")
+    loginForm.inputPassword("test")
+    loginForm.clickSignInButton()
 
-        loginForm.openLoginForm()
-        loginForm.inputUserName("test")
-        loginForm.inputPassword("test")
-        loginForm.clickSignInButton()
+    uiHelper.waitElement(pathToElement="//div[contains(@class, 'modal__paragraph form-error')]")
+    assert (uiHelper) == uiHelper
 
-        uiHelper.waitElement(pathToElement="//div[contains(@class, 'modal__paragraph form-error')]")
-        assert (uiHelper) == uiHelper
 
-    def testUsernameAndPassword_2(self):
-        self.getUrl("http://tvnew.domru.ru/")
-        uiHelper = UiHelper(self.driver)
-        loginForm = LoginFormPage(self.driver)
+def testUsernameAndPassword_login_2(setUp):
+    loginForm = LoginFormPage(setUp)
+    uiHelper = UiHelper(setUp)
 
-        loginForm.openLoginForm()
-        loginForm.inputUserName("test123")
-        loginForm.inputPassword("test123")
-        loginForm.clickSignInButton()
+    loginForm.openLoginForm()
+    loginForm.inputUserName("test123")
+    loginForm.inputPassword("test123")
+    loginForm.clickSignInButton()
 
-        uiHelper.waitElement(pathToElement="//div[contains(@class, 'modal__paragraph form-error')]")
-        assert (uiHelper) == uiHelper
+    uiHelper.waitElement(pathToElement="//div[contains(@class, 'modal__paragraph form-error')]")
+    assert (uiHelper) == uiHelper
 
-    def testUsernameAndPassword_3(self):
-        self.getUrl("http://tvnew.domru.ru/")
-        uiHelper = UiHelper(self.driver)
-        loginForm = LoginFormPage(self.driver)
 
-        loginForm.openLoginForm()
-        loginForm.inputUserName("тест")
-        loginForm.inputPassword("тест")
-        loginForm.clickSignInButton()
+def testUsernameAndPassword_login_3(setUp):
+    loginForm = LoginFormPage(setUp)
+    uiHelper = UiHelper(setUp)
 
-        uiHelper.waitElement(pathToElement="//div[contains(@class, 'modal__paragraph form-error')]")
-        assert (uiHelper) == uiHelper
+    loginForm.openLoginForm()
+    loginForm.inputUserName("тест")
+    loginForm.inputPassword("тест")
+    loginForm.clickSignInButton()
 
-    def testUsernameAndPassword__4(self):
-        self.getUrl("http://tvnew.domru.ru/")
-        uiHelper = UiHelper(self.driver)
-        loginForm = LoginFormPage(self.driver)
+    uiHelper.waitElement(pathToElement="//div[contains(@class, 'modal__paragraph form-error')]")
+    assert (uiHelper) == uiHelper
 
-        loginForm.openLoginForm()
-        loginForm.inputUserName("test")
-        loginForm.inputPassword("12345")
-        loginForm.clickSignInButton()
 
-        uiHelper.waitElement(pathToElement="//div[contains(@class, 'modal__paragraph form-error')]")
-        assert uiHelper == uiHelper
+def testUsernameAndPassword_login_4(setUp):
+    loginForm = LoginFormPage(setUp)
+    uiHelper = UiHelper(setUp)
 
-    def testUsernameAndPassword__5(self):
-        self.getUrl("http://tvnew.domru.ru/")
-        uiHelper = UiHelper(self.driver)
-        loginForm = LoginFormPage(self.driver)
+    loginForm.openLoginForm()
+    loginForm.inputUserName("test")
+    loginForm.inputPassword("12345")
+    loginForm.clickSignInButton()
 
-        loginForm.openLoginForm()
-        loginForm.inputUserName("tes")
-        loginForm.inputPassword("tes")
-        loginForm.clickSignInButton()
+    uiHelper.waitElement(pathToElement="//div[contains(@class, 'modal__paragraph form-error')]")
+    assert (uiHelper) == uiHelper
 
-        uiHelper.waitElement(pathToElement="//div[contains(@class, 'modal__paragraph form-error')]")
-        assert uiHelper == uiHelper
 
-    def testUsernameAndPassword__6(self):
-        self.getUrl("http://tvnew.domru.ru/")
-        uiHelper = UiHelper(self.driver)
-        loginForm = LoginFormPage(self.driver)
+def testUsernameAndPassword_login_5(setUp):
+    loginForm = LoginFormPage(setUp)
+    uiHelper = UiHelper(setUp)
 
-        loginForm.openLoginForm()
-        loginForm.inputUserName("te")
-        loginForm.inputPassword("te")
-        loginForm.clickSignInButton()
+    loginForm.openLoginForm()
+    loginForm.inputUserName("tes")
+    loginForm.inputPassword("tes")
+    loginForm.clickSignInButton()
 
-        uiHelper.waitElement(pathToElement="//div[contains(@class, 'modal__paragraph form-error')]")
-        assert uiHelper == uiHelper
+    uiHelper.waitElement(pathToElement="//div[contains(@class, 'modal__paragraph form-error')]")
+    assert (uiHelper) == uiHelper
+
+
+def testUsernameAndPassword_login_6(setUp):
+    loginForm = LoginFormPage(setUp)
+    uiHelper = UiHelper(setUp)
+
+    loginForm.openLoginForm()
+    loginForm.inputUserName("te")
+    loginForm.inputPassword("te")
+    loginForm.clickSignInButton()
+
+    uiHelper.waitElement(pathToElement="//div[contains(@class, 'modal__paragraph form-error')]")
+    assert (uiHelper) == uiHelper
+
+
+def testUsernameAndPassword_contractNumber_7(setUp):
+    loginForm = LoginFormPage(setUp)
+    uiHelper = UiHelper(setUp)
+
+    loginForm.openLoginForm()
+    loginForm.inputUserName("№test12345")
+    loginForm.inputPassword("test")
+    loginForm.clickSignInButton()
+
+    uiHelper.waitElement(pathToElement="//div[contains(@class, 'modal__paragraph form-error')]")
+    assert (uiHelper) == uiHelper
+
+
+def testUsernameAndPassword_contractNumber_8(setUp):
+    loginForm = LoginFormPage(setUp)
+    uiHelper = UiHelper(setUp)
+
+    loginForm.openLoginForm()
+    loginForm.inputUserName("№12345")
+    loginForm.inputPassword("test")
+    loginForm.clickSignInButton()
+
+    uiHelper.waitElement(pathToElement="//div[contains(@class, 'modal__paragraph form-error')]")
+    assert (uiHelper) == uiHelper
+
+
+def testUsernameAndPassword_contractNumber_9(setUp):
+    loginForm = LoginFormPage(setUp)
+    uiHelper = UiHelper(setUp)
+
+    loginForm.openLoginForm()
+    loginForm.inputUserName("00000")
+    loginForm.inputPassword("test")
+    loginForm.clickSignInButton()
+
+    uiHelper.waitElement(pathToElement="//div[contains(@class, 'modal__paragraph form-error')]")
+    assert (uiHelper) == uiHelper
+
+
+def testUsernameAndPassword_email_10(setUp):
+    loginForm = LoginFormPage(setUp)
+    uiHelper = UiHelper(setUp)
+
+    loginForm.openLoginForm()
+    loginForm.inputUserName("test@test.ru")
+    loginForm.inputPassword("test")
+    loginForm.clickSignInButton()
+
+    uiHelper.waitElement(pathToElement="//div[contains(@class, 'modal__paragraph form-error')]")
+    assert (uiHelper) == uiHelper
+
+
+def testUsernameAndPassword_email_11(setUp):
+    loginForm = LoginFormPage(setUp)
+    uiHelper = UiHelper(setUp)
+
+    loginForm.openLoginForm()
+    loginForm.inputUserName("test1@test.ru")
+    loginForm.inputPassword("test")
+    loginForm.clickSignInButton()
+
+    uiHelper.waitElement(pathToElement="//div[contains(@class, 'modal__paragraph form-error')]")
+    assert (uiHelper) == uiHelper
+
+
+def testUsernameAndPassword_email_12(setUp):
+    loginForm = LoginFormPage(setUp)
+    uiHelper = UiHelper(setUp)
+
+    loginForm.openLoginForm()
+    loginForm.inputUserName("test.test@test.ru")
+    loginForm.inputPassword("test")
+    loginForm.clickSignInButton()
+
+    uiHelper.waitElement(pathToElement="//div[contains(@class, 'modal__paragraph form-error')]")
+    assert (uiHelper) == uiHelper
